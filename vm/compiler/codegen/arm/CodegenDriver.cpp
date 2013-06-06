df85179ef26342b7b45932faa9d106a968830e98
aways <aways@outlook.com>
2013-06-06 22:02:58 +0200

    Revert "JIT: Hoist loop load operations above loop."
    This reverts commit 0706b74bc079365c9d5af203c23686cc8c6ed6ff.
    
    Conflicts:
    	vm/compiler/Loop.h

--- vm/compiler/codegen/arm/CodegenDriver.cpp
+++ vm/compiler/codegen/arm/CodegenDriver.cpp
@@ -3702,7 +3702,7 @@ static void handlePCReconstruction(CompilationUnit *cUnit,
      * We should never reach here through fall-through code, so insert
      * a bomb to signal troubles immediately.
      */
-    if ((numElems) || (cUnit->jitMode == kJitLoop)) {
+    if (numElems) {
         newLIR0(cUnit, kThumbUndefined);
     }
 
@@ -4357,7 +4357,6 @@ void dvmCompilerMIR2LIR(CompilationUnit *cUnit)
                                  (LIR *) cUnit->loopAnalysis->branchToBody);
             dvmCompilerAppendLIR(cUnit,
                                  (LIR *) cUnit->loopAnalysis->branchToPCR);
-            cUnit->loopAnalysis->branchesAdded = true;
         }
 
         if (headLIR) {
